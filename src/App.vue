ae3f552a69e1cba280a338e3f70bcd3d7e619d0d
Guilherme Nunes <guilhermelsnunes@gmail.com>
2022-03-08 08:02:05 +0000

    Add to List Exercise

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ greeting }}</h1>
+    <button v-on:click="updateGreeting">Change Greeting</button>
+    <ul>
+      <!-- <li v-for="(item, index) in items" :key="index">{{ item }}</li> -->
+      <li v-for="(item, index) in items" :key="index">
+        <span> {{ item.name}} </span>
+        <span v-if="item.isPurchased">Purchased!</span>
+      </li>
+    </ul>
+    <form v-on:submit.prevent="saveNewItem">
+      <label for="nem-item">Add a new Item:</label>
+      <input type="test" id="new-item" v-model="newItem"/>
+      <input type="submit" value="Save New Item"/>
+    </form>
+
+
   </div>
 </template>
 
+
+
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return{
+      greeting: "Hello World",
+      // items : ["Milk", "Cheese", "Beans"],
+
+      items:[
+        {name:"Milk", isPurchased: true },
+        {name:"Cheese", isPurchased: false },
+        {name:"Beans", isPurchased: true },
+      ],
+      newItem: "",
+    };
+  },
+  methods : {
+    updateGreeting : function(){
+      this.greeting = "Changed!";
+    },
+    saveNewItem: function(){
+      // this.items.push(this.newItem)
+            this.items.push({name:this.newItem, isPurchased: false});
+      this.newItem = "";
+    }
+  },
+  components: {},
+};
+
 </script>
+
+
+
+
+
 
 <style>
 #app {
